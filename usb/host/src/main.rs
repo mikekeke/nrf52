91ea254d15107e3dadd3214e395c958d50d8096c
Mikekekeke <>
2023-03-04 15:10:39 +0300

    ...

---
+++ usb/host/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("testing");
+    // let ports = serialport::available_ports().expect("No ports found!");
+    // for p in ports {
+    //     println!("{}", p.port_name);
+    // }
+
+    // let mut port = serialport::new("/dev/tty.usbmodemTEST1", 115_200)
+    //     .timeout(Duration::from_millis(100000))
+    //     .open()
+    //     .expect("Failed to open port");
+
+    // port.write(tx_id.to_bytes()).expect("Write failed!");
+
+    // let mut buf: Vec<u8> = vec![0; 64];
+    // port.read(buf.as_mut_slice()).expect("Found no data!");
+}
